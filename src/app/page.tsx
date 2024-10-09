a95fecf7090c762cc6ed22164792e18e1fe6967a
Bhushanmore25 <bhushanmore2582004@gmail.com>
2024-10-09 17:46:26 +0530

    login page work done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home(){
-  return <div>
+// Define the types for the session object
+export default function Component() {
+  const { data: session } = useSession();
 
-  </div>
+  // If the session exists, display the user's details and sign-out button
+  if (session) {
+    return (
+      <>
+        Signed in Name is {session.user?.name} <br />
+        Signed in email is {session.user?.email} <br />
+        {session.user?.image && <img src={session.user.image} alt="profile image" />}
+
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  // If the session does not exist, display the sign-in button
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn("google")}>Sign in with Google</button>
+    </>
+  );
 }
