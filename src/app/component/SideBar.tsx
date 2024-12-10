d2c26ef24556899af7961b0fb37ab50e2badc9ae
Piyush22070 <piyushbhoyar6480@gmail.com>
2024-12-10 19:27:04 +0530

    Sites Plan List made

--- src/app/component/SideBar.tsx
+++ src/app/component/SideBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import {FaMap, FaPage4 , FaBuilding, FaUserCircle, FaCog, FaSignInAlt, FaUserPlus, FaExclamationTriangle } from 'react-icons/fa';
+import { FaMap, FaPage4 , FaBuilding, FaUserCircle, FaCog, FaSignInAlt, FaUserPlus, FaExclamationTriangle } from 'react-icons/fa';
 
 const Sidebar = () => {
   return (
@@ -16,43 +16,42 @@ const Sidebar = () => {
         {/* Navigation Links */}
         <nav className="mt-6">
           <ul className="space-y-2">
-
-            <Link href="/" className="flex items-center px-6 py-2 hover:bg-blue-800">
-              <FaUserCircle className="mr-3" />
+            <Link href="/" className="flex items-center px-6 py-2 text-white focus:outline-none">
+              <FaUserCircle className="mr-3 " />
               <p>Overview</p>
             </Link>
 
-            <Link href="/BluePrint" className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <Link href="/SitePlans" className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaMap className="mr-3" />
-              <p>Blueprints</p>
+              <p>SitePlans</p>
             </Link>
             
-            <Link href="/Documents" className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <Link href="/Documents" className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaPage4 className="mr-3" />
               <p>Document</p>
             </Link>
 
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaBuilding className="mr-3" />
               <span>Companies</span>
             </li>
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaUserCircle className="mr-3" />
               <span>Account</span>
             </li>
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaCog className="mr-3" />
               <span>Settings</span>
             </li>
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaSignInAlt className="mr-3" />
               <span>Login</span>
             </li>
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaUserPlus className="mr-3" />
               <span>Register</span>
             </li>
-            <li className="flex items-center px-6 py-2 hover:bg-blue-800">
+            <li className="flex items-center px-6 py-2 text-white focus:outline-none">
               <FaExclamationTriangle className="mr-3" />
               <span>Error</span>
             </li>
